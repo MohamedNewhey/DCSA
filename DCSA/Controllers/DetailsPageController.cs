9a0f13a1591758d9b47343b0b957ac2ca364070f
mohanad Ayman <mohnd_aymn96@yahoo.com>
2022-04-09 04:00:51 +0200

    details page and edits

---
+++ DCSA/Controllers/DetailsPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DCSA.Controllers
+{
+    public class DetailsPageController : Controller
+    {
+        // GET: DetailsPage
+        public ActionResult DetailsPage()
+        {
+            return View();
+        }
+    }
+}
